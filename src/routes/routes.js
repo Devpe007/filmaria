ecf9b02abb6f62db10471f65b637d71f084766a1
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-11-02 17:01:07 -0300

    create Header component

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Header from '../components/Header';
+
 import Home from '../pages/Home';
 
 function Routes() {
     return (
         <BrowserRouter>
+         <Header />
          <Switch>
             <Route exact path="/" component={Home} />
          </Switch>
