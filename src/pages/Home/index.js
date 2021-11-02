f4d4ace9afb6c6396386fba5a9d2db515788c8af
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-11-02 17:20:54 -0300

    movie loading business rule created on Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from '../../services/api';
 
 function Home() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function loadMovies() {
+            const response = await api.get('/r-api/?api=filmes');
+            
+            setMovies(response.data);
+        };
+
+        loadMovies();
+    }, []);
+    
     return (
         <div>
             <h1>Página Home</h1>
