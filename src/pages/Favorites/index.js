9e6b70b515201237386d88ce52c775539bcbc1ab
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-11-03 20:52:23 -0300

    creted the structure of the Favorites page

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 
 function Favorites() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const myList = localStorage.getItem('movies');
+
+        setMovies(JSON.parse(myList) || []);
+    }, []);
+
     return (
-        <div>
-            <h1>Pagina Filmes Favoritos</h1>
+        <div id="my-movies" >
+            <h1>Meus Filmes</h1>
+
+            <ul>
+                {movies.map((item) => {
+                    return (
+                        <li key={item.id} >
+                            <span>{item.nome}</span>
+
+                            <div>
+                                <Link to={`/filme/${item.id}`} >Ver detalhes</Link>
+                                <button onClick={() => {}} >Excluir</button>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 };
