11f4384ebe1778b1a6fc1776fa6403a1cde9e89a
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-11-03 20:57:10 -0300

    create the style of the Favorites page

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
+import './styles.css';
+
 function Favorites() {
     const [movies, setMovies] = useState([]);
 
