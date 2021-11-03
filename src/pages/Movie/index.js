3edddad5c2cc331ecc4930968db767aadb467021
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-11-03 19:39:54 -0300

    create movie page loading logic

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,9 +1,38 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+
+import './styles.css';
+
+import api from '../../services/api';
 
 function Movie() {
+    const { id } = useParams();
+
+    const [movie, setMovie] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadMovie() {
+            const response = await api.get(`/r-api/?api=filmes/${id}`);
+            
+            setMovie(response.data);
+            setLoading(false);
+        };
+
+        loadMovie();
+    }, [id]);
+
+    if(loading) {
+        return (
+            <div className="movie-info" >
+                <h1>Carregando seu filme...</h1>
+            </div>
+        );
+    };
+
     return (
         <div className="movie-info" >
-            <h1>Página Detalhes</h1>
+            <h1>Página Detalhes - {id}</h1>
         </div>
     );
 };
