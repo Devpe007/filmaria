d1ffb2036f784fed57fb67a42b81bdbc6880e295
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-11-03 20:36:54 -0300

    create Favorites page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 
 import Home from '../pages/Home';
 import Movie from '../pages/Movie';
+import Favorites from '../pages/Favorites';
 
 function Routes() {
     return (
@@ -12,6 +13,7 @@ function Routes() {
          <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/filme/:id" component={Movie} />
+            <Route exact path="/favoritos" component={Favorites} />
          </Switch>
         </BrowserRouter>
     );
