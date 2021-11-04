c2d0e633ec1674ec9c3a4e7241ea10f5677d5bb9
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-11-03 23:08:53 -0300

    Error page structure created

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Error() {
     return (
-        <div>
+        <div className="not-found" >
             <h1>404</h1>
+            <h2>Página não encontrada!</h2>
+
+            <Link to="/" >Veja todos filmes</Link>
         </div>
     );
 };
